c3804127193bbbf15dfe4c6a083e4ce7fbd4e64b
Lian <176339768+Lian735@users.noreply.github.com>
2025-12-25 13:06:17 +0100

    Initial Commit

---
+++ Mouse Controller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mouse Controller
+//
+//  Created by Lian on 25.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mouse Controller/Mouse_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mouse_ControllerApp.swift
+//  Mouse Controller
+//
+//  Created by Lian on 25.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Mouse_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
